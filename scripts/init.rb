69ee13c2e89228b356d83d2077a8a0817fea1f65
Pixelastic <tim@pixelastic.com>
2014-07-20 16:21:03 +0200

    Fixing csv.

--- scripts/init.rb
+++ scripts/init.rb
@@ -10,17 +10,18 @@ class TEDTalksCSVToJSON
 
   def run
     data = Hash.new
-    CSV.foreach("../list.csv") do |line|
-      id = line[1]
-      data[id] = {
-        :id => id,
-        :url => line[2],
-        :speaker => line[3],
-        :name => line[4],
-        :summary => line[5],
-        :event => line[6],
-        :duration => line[7]
-      }
+    CSV.foreach("../src/audio-list.csv") do |line|
+      p line
+      # id = line[1]
+      # data[id] = {
+      #   :id => id,
+      #   :url => line[2],
+      #   :speaker => line[3],
+      #   :name => line[4],
+      #   :summary => line[5],
+      #   :event => line[6],
+      #   :duration => line[7]
+      # }
     end
     puts JSON.pretty_generate(data)
     
